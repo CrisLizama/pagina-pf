6596005666c334fcd488cd6fda293fa2cc0e1745
Cristian <cr.lizamal@gmail.com>
2026-04-28 16:31:14 -0400

    feat: attach training plan file to confirmation email

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ export default defineConfig({
   integrations: [react(), db()],
   vite: {
     server: {
-      allowedHosts: ['42d4-181-42-128-87.ngrok-free.app'] // debe cambiarse cada vez q inicio ngrock
+      allowedHosts: ['42d4-181-42-128-87.ngrok-free.app'] // debe cambiarse cada vez q inicio ngrock, en google cloud tmbn y en .env
+
     }
   }
 });
