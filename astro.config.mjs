045ef4f0db039d8b09896ca34c1b3f9972c82b83
Cristian <cr.lizamal@gmail.com>
2026-04-28 16:04:34 -0400

    feat: send confirmation email with Resend after purchase

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   integrations: [react(), db()],
   vite: {
     server: {
-      allowedHosts: ['2ae8-181-42-128-87.ngrok-free.app'] // debe cambiarse cada vez q inicio ngrock
+      allowedHosts: ['42d4-181-42-128-87.ngrok-free.app'] // debe cambiarse cada vez q inicio ngrock
     }
   }
 });
